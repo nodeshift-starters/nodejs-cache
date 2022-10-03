902cb854566c67d0b615735f065bb5feb26e4c28
Costas Papastathis <papastathiscr@gmail.com>
2022-10-03 15:19:49 +0300

    fix: deprecating proxyquire dependency

--- cute-name-service/test/cute-name-service-test.js
+++ cute-name-service/test/cute-name-service-test.js
@@ -2,24 +2,23 @@
 'use strict';
 
 const assert = require('assert');
-const proxyquire = require('proxyquire');
+const app = require('../app');
 const supertest = require('supertest');
 
-describe('Cute-name service', () => {
-  it('should have cute name response', async function () {
+describe('Cute-name service', async () => {
+  it('should have cute name response', async () => {
+    // this.timeout(0) disable timeout for this test
     this.timeout(0);
-    const app = proxyquire('../app', {
-      'project-name-generator': () => {
-        return {
-          spaced: 'cute-name'
-        };
-      }
-    });
+
     const response = await supertest(app)
       .get('/api/name')
       .expect('Content-Type', /html/)
       .expect(200);
 
-    assert.strictEqual(response.text, 'cute-name');
+    const isWordRegex = /^[a-zA-Z]+$/;
+    assert.match('responsetextsplit', isWordRegex);
+    assert.match(response.text.split(' ')[0], isWordRegex);
+    assert.match(response.text.split(' ')[1], isWordRegex);
+    assert.strictEqual(response.text.split(' ').length, 2);
   });
 });
